dd47a2716b18e143ccb5c315980a0ffed6f7078d
Flitig <flitig@kth.se>
2016-06-23 21:43:58 +0200

    added new failing test from specification

--- Tests/com/flitig/SystemTest.java
+++ Tests/com/flitig/SystemTest.java
@@ -22,8 +22,42 @@ public class SystemTest {
     @Test
     public void TestsAdding() throws Exception {
         IRegister register = new Register();
-        String[] input = new String[]{"add check account Bankkonto", "add expense account Livsmedel", "add expense account Hyra", "add income account Inkomst"};
-        String[] expected = new String[]{"Bankkonto:0", "Hyra:0", "Inkomst:0", "Livsmedel:0"};
+        String[] input = new String[]{
+                "add check account Bankkonto",
+                "add expense account Livsmedel",
+                "add expense account Hyra",
+                "add income account Inkomst"
+        };
+        String[] expected = new String[]{
+                "Bankkonto:0",
+                "Hyra:0",
+                "Inkomst:0",
+                "Livsmedel:0"
+        };
+        String[] actual = register.Run(input);
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void TestsAddingAndTransacting() throws Exception {
+        IRegister register = new Register();
+        String[] input = new String[]{
+                "add check account Bankkonto",
+                "add expense account Livsmedel",
+                "add expense account Hyra",
+                "add income account Inkomst",
+                "transaction from:Inkomst   to:Bankkonto 1000",
+                "transaction from:Bankkonto to:Livsmedel 50",
+                "transaction from:Bankkonto to:Hyra      250",
+                "transaction from:Inkomst   to:Bankkonto 1000",
+                "transaction from:Bankkonto to:Hyra      250"
+        };
+        String[] expected = new String[]{
+                "Bankkonto:1450",
+                "Hyra:-500",
+                "Inkomst:2000",
+                "Livsmedel:-50"};
         String[] actual = register.Run(input);
 
         assertArrayEquals(expected, actual);
