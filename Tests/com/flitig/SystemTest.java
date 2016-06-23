9273234777718a0c683f4f89909f06ff0d5a824e
Flitig <flitig@kth.se>
2016-06-23 21:37:32 +0200

    added one more failing test

--- Tests/com/flitig/SystemTest.java
+++ Tests/com/flitig/SystemTest.java
@@ -1,9 +1,8 @@
 package com.flitig;
 
-import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
 
 /**
  * Created by Flitig on 2016-06-23.
@@ -19,4 +18,18 @@ public class SystemTest {
         assertArrayEquals(expected, actual);
     }
 
+
+    @Test
+    public void TestsAdding() throws Exception {
+        IRegister register = new Register();
+        String[] input = new String[]{"add check account Bankkonto", "add expense account Livsmedel", "add expense account Hyra", "add income account Inkomst"};
+        String[] expected = new String[]{"Bankkonto:0", "Hyra:0", "Inkomst:0", "Livsmedel:0"};
+        String[] actual = register.Run(input);
+
+        assertArrayEquals(expected, actual);
+    }
 }
+
+
+
+
