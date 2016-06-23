4e9fddef9ea39568d348e6335799780bc5ae379a
Flitig <flitig@kth.se>
2016-06-23 23:48:28 +0200

    transaction tests

---
+++ Tests/com/flitig/TransactionTests.java
@@ -0,0 +1,78 @@
+package com.flitig;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Flitig on 2016-06-23.
+ */
+
+public class TransactionTests {
+    @Test
+    public void TestsTransactionFromCheckAccount() throws Exception {
+
+        // Specification: Saldot för check-konton ökar när kontot är ett to:-konto
+
+
+        IAccount account = new Account("Test");
+        account.SetAccountTypeStrategy(new CheckAccountStrategy());
+
+        account.TransactFrom(10);
+
+        int actual = account.getBalance();
+        int expected = -10;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void TestsTransactionToCheckAccount() throws Exception {
+
+        // Specification: Saldot för check-konton ökar när kontot är ett to:-konto
+
+        IAccount account = new Account("Test");
+        account.SetAccountTypeStrategy(new CheckAccountStrategy());
+        account.TransactTo(10);
+
+        int actual = account.getBalance();
+        int expected = 10;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void TestsTransactionFromBalanceAccount() throws Exception {
+
+        // Specification: Utgifts- och inkomstkonton däremot minskar när de är to:- konton.
+
+        IAccount account = new Account("Test");
+        account.SetAccountTypeStrategy(new BalanceAccountStrategy());
+
+        account.TransactFrom(10);
+
+        int actual = account.getBalance();
+        int expected = 10;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void TestsTransactionToBalanceAccount() throws Exception {
+
+        // Specification: Utgifts- och inkomstkonton däremot minskar när de är to:- konton.
+
+        IAccount account = new Account("Test");
+        account.SetAccountTypeStrategy(new BalanceAccountStrategy());
+
+        account.TransactTo(10);
+
+        int actual = account.getBalance();
+        int expected = -10;
+
+        assertEquals(expected, actual);
+    }
+
+}
+
