20f34fead34473583eaa5a03a538e41c3b7781ad
Flitig <flitig@kth.se>
2016-06-26 21:40:10 +0200

    since all tests pass, I created the main program that uses the implemented functionality

--- src/com/flitig/Main.java
+++ src/com/flitig/Main.java
@@ -1,10 +1,25 @@
 package com.flitig;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.TreeMap;
+import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) {
-        IRegister register = new Register(new AccountBuilder(), new TreeMap<>());
+        IRegister register;
+        BufferedReader br;
+        Stream<String> stream;
+        String[] output;
+
+        register = new Register(new AccountBuilder(), new TreeMap<>());
+
+        br = new BufferedReader(new InputStreamReader(System.in));
+        stream = br.lines();
+        output = register.Run(stream.toArray(String[]::new));
+
+        Arrays.stream(output).forEach(System.out::println);
     }
 }
